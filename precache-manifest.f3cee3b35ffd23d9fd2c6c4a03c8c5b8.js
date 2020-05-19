5f2033fc75a4f636ef2c963d04ffb2328d1b52e9
Arthur Chiang <mgshuter@gmail.com>
2020-05-20 01:57:33 +0800

    Updates

---
+++ precache-manifest.f3cee3b35ffd23d9fd2c6c4a03c8c5b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ce1431ba5f4fd26b2a9244d821b6774",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5c905c8014ab556140d4",
+    "url": "/monsters-rolodex/static/css/main.f774fa55.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c905c8014ab556140d4",
+    "url": "/monsters-rolodex/static/js/main.9f7f03f9.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
